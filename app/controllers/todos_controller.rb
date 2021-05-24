60cd10befa32ae6ac5962ecb2a7c7f22571280d3
Aravind <aravindhan25100p@gmail.com>
2021-05-24 13:30:28 +0530

    can see a single todo in /todo:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,10 @@ class TodosController < ApplicationController
   def index
     render plain: Todo.all.order(:due_date).map { |todo| todo.to_pleasent_string }.join("\n")
   end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_pleasent_string
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  # RESOURCES
+  # Todo is a resource
+  # - list of recources can be seen: index
+  # - single recource can be seen: show
+  # create a new resource
+
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 end
