45112ef68dea5377723255c2f82c1ddf07794e4c
Aravind <aravindhan25100p@gmail.com>
2021-05-25 12:58:48 +0530

    use the resources routing convention for todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,7 @@
 class TodosController < ApplicationController
+  #Skip verfying the authenticity token
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.order(:due_date).map { |todo| todo.to_pleasent_string }.join("\n")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-
-  # RESOURCES
-
-  # Todo is a resource
-  # - list of recources can be seen: index
-  # - single recource can be seen: show
-  # create a new todo
-
-  get "todos", to: "todos#index"
-  get "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
+  resources :todos
 end
