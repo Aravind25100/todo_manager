dc4184594d3b55ef41f710a8bb4b7df82e650a17
Aravind <aravindhan25100p@gmail.com>
2021-05-26 18:53:48 +0530

    Add availability to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
   def index
     render plain: User.all.map { |user| user.to_pleasent_string }.join("\n")
   end
+
+  def create
+    name, email, password = params[:name], params[:email], params[:password]
+    if (name != nil and password != nil and email != nil)
+      response_text = ""
+      if !User.find_by(email: email)
+        new_user = User.create!(name: name, email: email, password: password)
+        response_text = "User succesfully created with the id #{new_user.id}"
+      else
+        response_text = "Error! please try with different email account!"
+      end
+      render plain: response_text
+    else
+      render plain: "Error! Please fill all columns[name,email,password]!"
+    end
+  end
 end
