93afd97e37da4f7568bc2262c835596ad2a2a762
Aravind <aravindhan25100p@gmail.com>
2021-06-10 15:12:17 +0530

    Add sign-in, update session, fencing off protected routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def new
     render "users/new"
-  end
-
-  def index
-    render plain: User.all.map { |user| user.to_pleasent_string }.join("\n")
   end
 
   def create
@@ -17,14 +11,5 @@ class UsersController < ApplicationController
       password: params[:password],
     )
     redirect_to root_path
-  end
-
-  def login
-    email = params[:email]
-    password = params[:password]
-    user_email = User.find_by(email: email)
-    user_password = user_email != nil ? user_email.password : nil
-    response_text = user_password == password
-    render plain: response_text
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :todos
-
-  def to_pleasent_string
-    "#{id}. User: #{first_name} #{last_name} and e-mail: #{email}"
-  end
 end
