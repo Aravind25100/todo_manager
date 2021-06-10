ca148236eec3d785cc9e1222554296619f6fb89c
Aravind <aravindhan25100p@gmail.com>
2021-06-10 12:40:26 +0530

    Add user sign-in functionality (without session)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+    render "new"
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if (user && user.authenticate(params[:password]))
+      render plain: "Correct password!"
+    else
+      render plain: "Incorrect password!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   post "users/login", to: "users#login"
   get "/" => "home#index"
   root to: "home#index"
+
+  get "/signin", to: "sessions#new", as: :new_sessions
+  post "/signin", to: "sessions#create", as: :sessions
 end
