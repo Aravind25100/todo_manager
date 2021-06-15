03888dd87b68b4d22704cada906b123aa305e5ab
Aravind <aravindhan25100p@gmail.com>
2021-06-15 15:27:40 +0530

    Using validates to check the email is already taken or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    if User.find_by(email: params[:email])
-      flash[:error] = "Email Id already taken!"
-      redirect_to new_user_path
-    else
       user = User.new(
         first_name: params[:first_name],
         last_name: params[:last_name],
@@ -24,6 +20,5 @@ class UsersController < ApplicationController
         flash[:error] = user.errors.full_messages.join(", ")
         redirect_to new_user_path
       end
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, { presence: true, uniqueness: true }
   validates :first_name, length: { minimum: 4 }
 
   has_many :todos
